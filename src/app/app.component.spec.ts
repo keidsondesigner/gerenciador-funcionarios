893dfe29dbada2265ea1f267f470f59c2a61a513
keidsondesigner <keidsondesigner@gmail.com>
2022-11-27 20:08:58 -0400

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gerenciamentoFuncionarios'`, () => {
+  it(`should have as title 'gerenciadorFuncionarios'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gerenciamentoFuncionarios');
+    expect(app.title).toEqual('gerenciadorFuncionarios');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gerenciamentoFuncionarios app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gerenciadorFuncionarios app is running!');
   });
 });
